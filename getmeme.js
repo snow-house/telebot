03e33854cf5d046a23a0946f3a8b74b4a6983245
aryuuu <herppratama@gmail.com>
2019-12-25 15:25:08 +0700

    testing snoowrap

---
+++ getmeme.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const snoowrap = require("snoowrap");
+
+async function scrapeSubreddit() {
+
+	const r = new snoowrap({
+			userAgent : "fatt",
+			clientId : process.env.REDDITCLIENTID,
+			clientSecret : process.env.REDDITCLIENTSECRET,
+			refreshToken : process.env.REDDITREFRESHTOKEN
+	});
+	
+	var subreddit = await r.getSubreddit("dankmemes");
+	var topPosts = await subreddit.getTop({time: "day", limit: 100});
+	
+	let data = [];
+
+	topPosts.forEach(post => {
+			
+			data.push({
+				link: post.url,
+				text: post.title,
+				score: post.score
+			})
+	});
+	// console.log(data);
+	return data
+}
+
+// scrapeSubreddit()
+module.exports.scrapeSubreddit = scrapeSubreddit;
