19fd65e7c0fe3ad784c5edf3d095f2967dc2d946
M Algah Fattah Illahi <herppratama@gmail.com>
2020-05-07 12:57:13 +0700

    new template

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ const opts = {
 };
 const vvimg = process.env.VVIMG;
 const fbimg = process.env.FBIMG;
-const blank = process.env.BLANK;
+const blankimg = process.env.BLANK;
 
 // constants
 const internalError = "Something went wrong :("
@@ -629,36 +629,41 @@ bot.onText(/{([^{}])+}/, (msg, match) => {
 });
 
 bot.onText(/<([^<>])+>/, (msg, match) => {
+
 	const chatId = msg.chat.id;
 	const textData = {
-		text: match[0].substring(1,match[0].length-1),
-		maxWidth: 380,
-		maxHeight: 250,
-		x: 178,
-		y: 465
+	    text: match[0].substring(1,match[0].length-1),
+	    maxWidth: 380,
+	    maxHeight: 250,
+	    x: 178,
+	    y: 465
 	};
 
 	jimp.read(fbimg)
 	.then(image => {
-		jimp.loadFont(jimp.FONT_SANS_32_BLACK)
-		.then(font => {
-			image.print(
-				font,
-				textData.x,
-				textData.y,
-				{
-					text: textData.text,
-					alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
-					alignmentY: jimp.VERTICAL_ALIGN_MIDDLE
-				},
-				textData.maxWidth,
-				textData.maxHeight
-			)
-			.getBuffer(jimp.MIME_JPEG, (err, buffer) => {
-				bot.sendPhoto(chatId, buffer);
-			})
-			
-		})
+	    jimp.read(blankimg)
+	    .then(blank => {
+	        jimp.loadFont(jimp.FONT_SANS_32_BLACK)
+	        .then(font => {
+	            blank.print(
+	                font,
+	                0,
+	                0,
+	                {
+	                    text: textData.text,
+	                    alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
+	                    alignmentY: jimp.VERTICAL_ALIGN_MIDDLE
+	                },
+	                300,
+	                220
+	            )
+	            .rotate(-6)
+	            image.composite(blank, 261, 489, [jimp.BLEND_DESTINATION_OVER, 0.2, 0.2])
+	            .getBuffer(jimp.MIME_JPEG, (err, buffer) => {
+	            	bot.sendPhoto(chatId, buffer);
+	            })
+	        })
+	    })
 	})
 	.catch(err => {
 		console.log(err);
